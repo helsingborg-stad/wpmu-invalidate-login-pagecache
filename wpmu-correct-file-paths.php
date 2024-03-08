4276a7ef49b803efbc322af5e4b47f13e4e0d1de
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2024-03-08 09:18:14 +0100

    feat: init

---
+++ wpmu-correct-file-paths.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+Plugin Name:    WPMU Invalidate Login Pagecache
+Description:    -
+Version:        1.0.0
+Author:         Sebastian Thulin
+*/
+
+namespace WPMUInvalidateLoginPagecache;
+
+class WPMUInvalidateLoginPagecache
+{
+    public function __construct()
+    {
+    }
+
+    public function func($a)
+    {
+    }
+}
+
+new \WPMUInvalidateLoginPagecache\WPMUInvalidateLoginPagecache();
